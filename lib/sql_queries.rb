5792368fa610e400321fba7b6c4e9b8fc474fe4c
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-31 17:11:23 -0400

    pass query 2

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,7 +18,12 @@ ORDER BY projects.title
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT users.name, users.age, SUM(pledges.amount)
+FROM pledges
+INNER JOIN users
+ON users.id = pledges.user_id
+GROUP BY users.name
+ORDER BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
